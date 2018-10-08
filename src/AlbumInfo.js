01312ef8d61de340fd7f6a8b2ad741b7d13d6253
Lucas Carvalho <lucasccorrea@gmail.com>
2018-10-08 15:04:53 -0300

    fix error lint

--- src/AlbumInfo.js
+++ src/AlbumInfo.js
@@ -1,5 +1,6 @@
 export default function renderAlbumInfo(data, element) {
-  element.innerHTML = `<img class="album-image" src="${data.images[0].url}" alt="">
+  const elementTarget = element;
+  elementTarget.innerHTML = `<img class="album-image" src="${data.images[0].url}" alt="">
     <p class="album-title">${data.name}</p>
     <p class="album-artist">${data.artists[0].name}</p>
     <p class="album-counter">${data.tracks.total} Músicas</p>`;
